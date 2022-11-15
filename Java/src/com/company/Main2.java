ac0432861212d5daaaed02d907cf8b9f12b0fade
archiechop1 <alenka.dzyum1@gmail.com>
2022-11-15 20:08:43 +0200

    Types of variables

---
+++ Java/src/com/company/Main2.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main2 {
+    public static void main(String[] args){
+        int number = 10;
+        //byte [-128, 127)
+        byte number2 = 15;
+        float number3 = 5.5f;
+        double number4 = 6.5d;
+        char symbol = '+';
+        String str = "Hello";
+        boolean isTrue = true;
+
+        System.out.println(number);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        System.out.println(symbol);
+        System.out.println(str);
+        System.out.println(isTrue);
+    }
+}
