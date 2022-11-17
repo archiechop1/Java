a95eba69389c4ce49b0b824bc104b99c254a5669
archiechop1 <alenka.dzyum1@gmail.com>
2022-11-17 20:14:15 +0200

    Switch case

--- Java/src/com/company/Main8.java
+++ Java/src/com/company/Main8.java
@@ -1,4 +1,25 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main8 {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        int num = scn.nextInt();
+
+        switch (num){
+            case 10:
+                System.out.println("num=10");
+                break;
+            case 20:
+                System.out.println("num=20");
+                break;
+            case 100:
+                System.out.println("num=100");
+                break;
+            default:
+                System.out.println("Num is some number");
+    }
+
+    }
 }
