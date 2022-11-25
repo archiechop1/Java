a43c132d0ac161d7065dd5c7c4d83481b10acade
archiechop1 <alenka.dzyum1@gmail.com>
2022-11-26 00:22:01 +0200

    homework2

---
+++ Java/src/com/company/Homework2.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        int[] array = {1, 2, 3, 4, 5};
+        array[4] = 10;
+            for (int i=0; i< array.length; i++ ) {
+                System.out.print(array[i] + " ");
+            }
+    }
+}
