066003b9aa20b88f3eeea90ad721cf7a3c345e4d
archiechop1 <alenka.dzyum1@gmail.com>
2022-12-01 17:33:42 +0200

    hw3

--- Java/src/com/company/Homework3.java
+++ Java/src/com/company/Homework3.java
@@ -7,13 +7,20 @@ public class Homework3 {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5};
         int[] another = new int[numbers.length];
-        for (int i = 0; i < numbers.length; i++) {
-            another[numbers.length - 1 - i] = numbers[i];
+        int count=0;
+        for (int i = numbers.length - 1; i > -1; i--) {
+            another[count] = numbers[i];
+            count++;
         }
-        numbers = another;
-        for (int i : numbers) {
 
-            System.out.print(" " +i);
+
+//            another[numbers.length - 1 - i] = numbers[i];
+//        }
+//        numbers = another;
+//        for (int i : numbers) {
+//
+//            System.out.print(" " +i);
+//        }
+            System.out.println(Arrays.toString(another));
         }
     }
-}
